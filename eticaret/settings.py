7c7f0bd86f6577d261f3fc4384dc88cc85b7f570
canerucal <caner.ucal@gmail.com>
2022-10-13 12:42:24 +0300

    settings

--- eticaret/settings.py
+++ eticaret/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = os.environ.get('secret') #herokuya salarken bunu yap
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('secret') #herokuya salarken bunu yap
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
-# DEBUG = os.environ.get('debug') #herokuya salarken bunu yap
+# DEBUG = config('DEBUG')
+DEBUG = os.environ.get('debug') #herokuya salarken bunu yap
 
 ALLOWED_HOSTS = []
 
