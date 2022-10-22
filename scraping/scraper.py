8a5b9200580f16576299cb337d31b172c1885edc
canerucal <caner.ucal@gmail.com>
2022-10-22 14:04:00 +0300

    finished

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,13 +1,18 @@
+#bu python dosyası, trendyol ve n11 sitelerinden veri kazıma işlemi için yazılmıştır.
+
+#kütüphanelerin import edilmesi
 import requests, re, json
 from bs4 import BeautifulSoup
 import psycopg2
 import time
 
+#kodun toplam çalışma süresini çıkarmak amaçlı timer başlatılması
 start_time = time.time()
 
 print('\nBu program Trendyol ve n11 üzerinden veri kazıma işlemi yapmaktadır.\n')
 print('İşlem internet ve işlemci hızına göre değişkenlik göstermekle birlikte yaklaşık olarak 500 saniye sürmektedir. \n')
 
+#veritabanı bağlantısının gerçekleştirilmesi
 def dbConn():
     global connection, cursor
     connection = psycopg2.connect(
@@ -20,11 +25,13 @@ def dbConn():
     cursor = connection.cursor()
     print('Veri tabanı bağlantısı gerçekleştiriliyor....\n')
 
-
+#trendyol veri kazıma fonksiyonu
 def getTrendyolData():
     dbConn()
     main_url = "https://www.trendyol.com/laptop-x-c103108"
     print('Trendyol veri kazıma işlemi başladı.')
+    
+    #sayfa geçişleri için for döngüsü kurulması
     for a in range(1,10):
         print('Veri kazıma işlemi devam ediyor.')
         if a == 5:
@@ -37,14 +44,15 @@ def getTrendyolData():
         url = main_url+'?pi={}'.format(a)
         page = requests.get(url) #html request
         soup = BeautifulSoup(page.content, "html.parser") #sayfa içeriğinin parse edilmesi
-        tags = soup.find("div", {'class':"prdct-cntnr-wrppr"}).find_all('a') #ürün linklerinin alınması a taglerinin çekilmesi
+        tags = soup.find("div", {'class':"prdct-cntnr-wrppr"}).find_all('a') #sayfadaki tüm ürün linklerinin alınması
 
-        # https://cdn.dsmcdn.com/ty518/product/media/images/20220826/13/165993243/230886343/1/1_org_zoom.jpg
-        # https://cdn.dsmcdn.com/mnresize/128/192/ty518/product/media/images/20220826/13/165993243/230886343/1/1_org_zoom.jpg
+        #ürün linklerinin içindeki bilgileri almak için for döngüsü kurulması
         for tag in tags:
             tag = tag['href']
             r = requests.get('https://www.trendyol.com'+tag)
             soup2 = BeautifulSoup(r.content, "html.parser")
+
+            #ürün görsel linklerinin alınması
             for item in soup2.find_all('img'):
                 if str(item).find('mnresize') > 0:
                     item['src'] = item['src'].replace('/mnresize','')
@@ -56,17 +64,15 @@ def getTrendyolData():
 
             r = r.text
 
-            #trendyolda 2 farklı js regex ifadesi var. bu sebeple 2 match koşulu yazıldı.
+            #verilerin json formatında çekilmesi
             matches1 = re.search(r"window.__PRODUCT_DETAIL_APP_INITIAL_STATE__=({.*}});window", r)
             matches2 = re.search(r"window.__PRODUCT_DETAIL_APP_INITIAL_STATE__\s=\s({.*}})", r)
-
             if matches1 is not None:
                 matches = matches1
             elif matches2 is not None:
                 matches = matches2
             else:
                 print('Lütfen regex ifadelerini veya json data kaynaklarını kontrol edin.')
-
             if matches is not None:
                 json_data = json.loads(matches.group(1))
                 global brand, model_name, model_no, price, point, website, os, cpu, cpu_gen, ram, ssd_size, hdd_size,screen_size
@@ -74,9 +80,10 @@ def getTrendyolData():
                 brand = (json_data['product']['brand']['name']).upper() #marka adı
                 model_name = (json_data['product']['name'].upper()) #model adı
                 model_no = (json_data['product']['productCode'].upper()) #model no
-                price =  (json_data['product']['price']['discountedPrice']['value'])
-                point =  (json_data['product']['ratingScore']['averageRating']) #buradan puana git
+                price =  (json_data['product']['price']['discountedPrice']['value']) #ürün fiyatı
+                point =  (json_data['product']['ratingScore']['averageRating']) #ürün puanı
                 website = 'trendyol'
+                #ürünler bilgilerine id detayından ulaşılması
                 for i in json_data['product']['attributes']:
                     attr_check = list(list(i.values())[0].values())[1]
                     if attr_check == 28:
@@ -94,6 +101,7 @@ def getTrendyolData():
                     elif attr_check == 23:
                         screen_size = list(list(i.values())[1].values())[0].upper()
 
+                        #near duplicate kontrolü yapılması
                         duplicate_check = """select url from brand where url='"""+tag+"""'"""
                         cursor.execute(duplicate_check)
                         try:
@@ -102,9 +110,11 @@ def getTrendyolData():
                         except:
                             record = " "
 
+                        #veritabanına kayıt için string veritipine dönüşüm.
                         point = str(point)
                         price = str(price)
-
+                        
+                        #near duplicate kontrolüne devam ediliyor.
                         if record == tag:
                             print('aynı kayıt olduğu için kaydedilmedi.', tag)
                             print('-------')
@@ -114,7 +124,8 @@ def getTrendyolData():
                             price_record = cursor.fetchall()[0][0]
                             cursor.execute(point_check)
                             point_record = cursor.fetchall()[0][0]
-
+                            
+                            #çekilen verideki price veya point değerleri veritabanındakinden farklıysa update işlemi yapılıyor.
                             if price != price_record:
                                 print('fiyat güncelleniyor!')
                                 new_price = price
@@ -135,11 +146,13 @@ def getTrendyolData():
                             connection.commit()
                             print(brand, model_no, 'pc kaydedildi.')
                             print('------')
-        
+
+#n11 veri kazıma fonksiyonu
 def getn11Data():
     dbConn()
     main_url = "https://www.n11.com/bilgisayar/dizustu-bilgisayar"
     print('n11 veri kazıma işlemi başladı.')
+    #sayfa geçişleri için for döngüsünün kurulması
     for a in range(1,10):
         print('Veri kazıma işlemi devam ediyor.')
         if a == 5:
@@ -151,12 +164,16 @@ def getn11Data():
         url = main_url+'?pg={}'.format(a)
         page = requests.get(url) #html request
         soup = BeautifulSoup(page.content, "html.parser") #sayfa içeriğinin parse edilmesi
-        tags = soup.find("div", {'class':"catalogView"}).find_all('a')
+        tags = soup.find("div", {'class':"catalogView"}).find_all('a') #ürün linklerinin çekilmesi
 
+        #ürün linklerinin içindeki bilgileri almak için for döngüsü kurulması
         for tag in tags:
             tag = tag['href']
             r = requests.get(tag)
             soup2 = BeautifulSoup(r.content, "html.parser")
+
+            #n11 veri alınması engellemek için link forwardlama yapmakta.
+            #link forwardlama kontrolü yapılması.
             fw_check = soup2.find('div', {'class': 'filterArea'})
             if fw_check is not None:
                 print('n11 link forwardlayarak önlem almakta. Bu sebeple veri çekilemiyor.')
@@ -175,33 +192,34 @@ def getn11Data():
                     k = k.text
                     value_data.append(k)
 
-
+                #elde edilen verilerin dictionary formatına dönüştürülmesi.
+                #ürün özelliklerinin çekilmesi
                 dictionary = dict(zip(key_data, value_data))
                 brand_n11 = (dictionary['Marka'][1:]).upper()
-                model_name_n11 = soup2.find('h1', {'class': 'proName'})
+                model_name_n11 = soup2.find('h1', {'class': 'proName'}) #model adı
                 if model_name_n11 is None:
                     model_name_n11 = "Bilgi yok"
                 else:
                     model_name_n11 = model_name_n11.text
-                photo_n11 = soup2.find('div', {'class': 'imgObj'}).find('a')['href']
+                photo_n11 = soup2.find('div', {'class': 'imgObj'}).find('a')['href'] #ürün görseli
                 if photo_n11 is None:
                     photo_n11 = "Bilgi yok"
-                model_name_n11 = " ".join(model_name_n11.split()).upper()
-                model_no_n11 = dictionary['Model'][1:].upper()
-                os_n11 = dictionary['İşletim Sistemi'][1:].upper()
-                cpu_n11 = dictionary['İşlemci'][1:].upper()
-                cpu_gen_n11 = dictionary['İşlemci Modeli'][1:].upper()
-                ram_n11 = dictionary['Bellek Kapasitesi'][1:].upper()
-                disk_capacity_n11 = dictionary['Disk Kapasitesi'][1:].upper()
-                screen_size_n11 = dictionary['Ekran Boyutu'][1:].upper()
-                point_n11 = soup2.find('strong', {'class': 'ratingScore r100'})
+                model_name_n11 = " ".join(model_name_n11.split()).upper() #model adı
+                model_no_n11 = dictionary['Model'][1:].upper() #model numarası
+                os_n11 = dictionary['İşletim Sistemi'][1:].upper() #işletim sistemi
+                cpu_n11 = dictionary['İşlemci'][1:].upper() #cpu
+                cpu_gen_n11 = dictionary['İşlemci Modeli'][1:].upper() #cpu nesli
+                ram_n11 = dictionary['Bellek Kapasitesi'][1:].upper() #ram
+                disk_capacity_n11 = dictionary['Disk Kapasitesi'][1:].upper() #disk kapasitesi
+                screen_size_n11 = dictionary['Ekran Boyutu'][1:].upper() #ekran ölçüsü
+                point_n11 = soup2.find('strong', {'class': 'ratingScore r100'}) #ürün puanı
                 if point_n11 is None:
                     point_n11 = "Bilgi yok"
                     point_n11 = str(point_n11)
                 else:
                     point_n11 = point_n11.text
                     point_n11 = str(point_n11)
-                price_n11 = soup2.find('div', {'class': 'unf-p-summary-price'})
+                price_n11 = soup2.find('div', {'class': 'unf-p-summary-price'}) #ürün fiyatı
                 if price_n11 is None:
                     price_n11 = "Bilgi yok"
                     price_n11 = str(price_n11)
@@ -233,6 +251,7 @@ def getn11Data():
                 elif screen_size_n11 is None:
                     screen_size_n11 = "Bilgi yok"
 
+                #near duplicate kontrolü
                 duplicate_check2 = """select url from brand where url='"""+tag+"""'"""
                 cursor.execute(duplicate_check2)
                 try:
@@ -240,7 +259,7 @@ def getn11Data():
                     record2 = cursor.fetchall()[0][0]
                 except:
                     record2 = " "
-
+                
                 if record2 == tag:
                     print('aynı kayıt olduğu için kaydedilmedi.', tag)
                     print('-------')
@@ -251,6 +270,7 @@ def getn11Data():
                     cursor.execute(point_check)
                     point_record = cursor.fetchall()[0][0]
 
+                    #çekilen verideki price veya point değerleri veritabanındakinden farklıysa update işlemi yapılıyor.
                     if price_n11 != price_record:
                         print('fiyat güncelleniyor!')
                         new_price = price_n11
@@ -272,14 +292,16 @@ def getn11Data():
                     print(brand_n11, model_no_n11, 'pc kaydedildi.')
                     print('---------')
 
+#veri kazıma fonksiyonlarının çalıştırılması
 getTrendyolData()
 print('Trendyol kazıma işlemi tamamlandı.')
 getn11Data()
 print('n11 kazıma işlemi tamamlandı.')
 
-
+#veritabanı bağlantısının kapatılması
 cursor.close()
 connection.close()
 
+#sayacın durdurulması
 stop_time = time.time()
 print("The time of the run:", stop_time - start_time)
